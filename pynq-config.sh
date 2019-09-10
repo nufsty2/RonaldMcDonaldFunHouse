23cf328c7d064e492012b2a5f5c31e672853bf66
nufsty2 <nufsty2@gmail.com>
2019-09-09 19:04:01 -0600

    Change recursive delete

--- pynq-config.sh
+++ pynq-config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "PYNQ ##:"
 read pynqnum
-rm -r -f ~/pynq_mnt
+umount ~/pynq_mnt
+rmdir ~/pynq_mnt
 mkdir ~/pynq_mnt
 sshfs xilinx@pynq$pynqnum.ee.byu.edu:/home/xilinx ~/pynq_mnt
 ssh "xilinx@pynq$pynqnum.ee.byu.edu"
