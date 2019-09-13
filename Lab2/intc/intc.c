5cdeb8020b3fd80e4f706d5603469b01419a3ed9
Jeff DeGraw <nufsty2@gmail.com>
2019-09-13 14:38:24 -0600

    Started?

--- Lab2/intc/intc.c
+++ Lab2/intc/intc.c
@@ -1,6 +1,11 @@
     #include <stdint.h>
+    #include <unistd.h>
+    #include <fcntl.h>
+    #include <sys/mman.h>
      
     #define INTC_SUCCESS 0 
+
+    static int file;
      
     // Initializes the driver (opens UIO file and calls mmap)
     // devDevice: The file path to the uio dev file
@@ -8,19 +13,55 @@
     // This must be called before calling any other intc_* functions
     int32_t intc_init(char devDevice[])
     {
-        mmap();
-        fdgg
-        fd
-        
+        file = open(devDevice, O_RDWR);
+
+        if (file == UIO_EXAMPLE_ERROR)
+        {
+            //file descriptors have to be > 0 to be valid
+            return UIO_EXAMPLE_ERROR;
+        }
+
+        //memory map the physical address of the hardware into virtual address space
+        ptr = mmap(NULL, UIO_EXAMPLE_MMAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, file, MMAP_OFFSET);
+        if (ptr == MAP_FAILED)
+        {
+            return UIO_EXAMPLE_ERROR;
+        }
+
+        /* put hardware setup here */
+
+        return UIO_EXAMPLE_SUCCESS;
     }
-     
+
+    // write to a register of the UIO device
+    void generic_write(uint32_t offset, uint32_t value)
+    {
+        //the address is cast as a pointer so it can be dereferenced
+        *((volatile uint32_t *)(ptr + offset)) = value;
+    }
+
+    // read from a register of the UIO device
+    uint32_t generic_read(uint32_t offset)
+    {
+        return *((volatile uint32_t *)(ptr + offset));
+    }
+
+    // close the UIO device
+    //	this function must be called after all read/write operations are done
+    //	to properly unmap the memory and close the file descriptor
+    void generic_exit()
+    {
+        munmap(ptr, UIO_EXAMPLE_MMAP_SIZE);
+        close(file);
+    }
+
     // Called to exit the driver (unmap and close UIO file)
     void intc_exit()
     {
 
     }
      
-    // This function will block until an interrupt occurrs
+    // This function will block until an interrupt occurs
     // Returns: Bitmask of activated interrupts
     uint32_t intc_wait_for_interrupt()
     {
