2648bc2341818b539ba1b3c3c2edfda255e1223f
drewtdowling <drewdowling97@gmail.com>
2019-09-24 18:54:18 -0600

    Updated @6:55pm MST

--- Lab2/main/main.c
+++ Lab2/main/main.c
@@ -3,79 +3,181 @@
     #include "../buttons/buttons.h"
     #include "../switches/switches.h"
     #include <stdio.h>
+    #include <stdbool.h>
 
     #define FIT_MASK 0x1 // 001
     #define BTN_MASK 0x2 // 010
-    #define SW_MASK  0x3 // 011
+    #define SW_MASK  0x4 // 100
+
+    #define BTN_0 0x1
+    #define BTN_1 0x2
+    #define BTN_2 0x4
+    #define SW_0  0x1
 
     #define DEBOUNCE_MAX_VAL 3
 
+    #define SEC 0
+    #define MIN 1
+    #define HR  2
+
     uint32_t fit_ctr = 0;
-    uint32_t second_ctr = 0;
+
+    int32_t second_ctr = 10;
+    int32_t minute_ctr = 1;
+    int32_t hour_ctr = 00;
+
     uint32_t debounce_ctr = 0;
     uint32_t buttons_val = 0;
     uint32_t new_buttons_val = 0;
     uint32_t switches_val = 0;
     uint32_t new_switches_val = 0;
+
+    void print_time() 
+    {
+        printf("\r%02d:%02d:%02d", hour_ctr, minute_ctr, second_ctr);
+        fflush(stdout);
+    }
+
+    void advance_time(int16_t unit, bool counting)
+    {
+        switch (unit)
+        {
+        case SEC:
+            second_ctr++;
+            break;
+        case MIN:
+            minute_ctr++;
+            break;
+        case HR:
+            hour_ctr++;
+            break;
+        default:
+            break;
+        }
+
+        fit_ctr = 0;
+
+        if (second_ctr > 59)
+        {
+            if (counting)
+                minute_ctr++;
+
+            second_ctr = 0;
+        }
+        
+        if (minute_ctr > 59)
+        {
+            if (counting)
+                hour_ctr++;
+
+            minute_ctr = 0;
+        }
+
+        if (hour_ctr > 23)
+        {
+            if (counting)
+            {
+                second_ctr = 0;
+                minute_ctr = 0;
+            }
+            hour_ctr = 0;
+        }
+        print_time();
+    }
+
+    void reverse_time(int16_t unit)
+    {
+        switch (unit)
+        {
+        case SEC:
+            second_ctr--;
+            break;
+        case MIN:
+            minute_ctr--;
+            break;
+        case HR:
+            hour_ctr--;
+            break;
+        default:
+            break;
+        }
+
+        fit_ctr = 0;
+
+        if (second_ctr < 0) 
+        {
+            second_ctr = 59;
+        }
+
+        if (minute_ctr < 0) 
+        {
+            minute_ctr = 59;
+        }
+
+        if (hour_ctr < 0)
+        {
+            hour_ctr = 23;
+        }
+        print_time();
+    }
+
+    void set_time() {
+        if (buttons_val == (BTN_0 | BTN_1) ||
+            buttons_val == (BTN_0 | BTN_2) ||
+            buttons_val == (BTN_1 | BTN_2) ||
+            buttons_val == (BTN_0 | BTN_1 | BTN_2))
+            {
+                return;
+            }
+
+        switch(buttons_val) {
+            case BTN_0:
+                if (switches_val == SW_0) {
+                    advance_time(SEC, false);
+                }
+                else
+                {
+                    reverse_time(SEC);
+                }
+                break;
+            case BTN_1:
+                if (switches_val == SW_0) {
+                    advance_time(MIN, false);
+                }
+                else
+                {
+                    reverse_time(MIN);
+                }
+                break;
+            case BTN_2:
+                if (switches_val == SW_0) {
+                    advance_time(HR, false);
+                }
+                else
+                {
+                    reverse_time(HR);
+                }
+                break;
+            default:
+                return;
+        }
+    }
      
     // This is invoked in response to a timer interrupt.
     // It does 2 things: 1) debounce switches, and 2) advances the time.
     void isr_fit()
     {
-        // Advanced the time
-        if (++fit_ctr == 100)
-        {
-            second_ctr++;
-            fit_ctr = 0;
-        }
-
         if (++debounce_ctr >= DEBOUNCE_MAX_VAL)
         {
             debounce_ctr = 0;
             buttons_val = new_buttons_val;
             switches_val = new_switches_val;
-        
-            if (buttons_val == 1)
-            {
-                printf("\rButton 0 Pressed");
-            }
-            else if (buttons_val == 2)
-            {
-                printf("\rButton 1 Pressed");
-            }
-            else if (buttons_val == 4)
-            {
-                printf("\rButton 2 Pressed");
-            }
-            else if (buttons_val == 8)
-            {
-                printf("\rButton 3 Pressed");
-            }
-            else 
-            {
-                printf("\rNo button pressed!");
-            }
+        }
 
-            if (switches_val == 1)
-            {
-                printf("\rSwitch 1 activated!");
-                fflush(stdout);
-            }
-            else if (switches_val == 2)
-            {
-                printf("\rSwitch 2 activated!");
-                fflush(stdout);
-            }
-            else if (switches_val == 3)
-            {
-                printf("\rBoth SW activated!");
-                fflush(stdout);
-            }
-            else
-            {
-                printf("\rNo switch activated!");
-                fflush(stdout);
-            }
+        // Advanced the time
+        if (++fit_ctr == 100)
+        {
+            advance_time(SEC, true);
         }
     }
      
@@ -85,7 +187,10 @@
         debounce_ctr = 0;
 
         new_buttons_val = read_buttons();
+
         clear_button_interrupts();
+
+        set_time();
     }
 
     void isr_switches()
