99a9a78d4d6299e5441a92162f24c5df5865e743
nufsty2 <nufsty2@gmail.com>
2019-10-08 21:04:29 -0600

    THIS IS SUTPED

--- Lab3/main/main.c
+++ Lab3/main/main.c
@@ -41,6 +41,7 @@
 #define RIGHT_EDGE (NEW_LINE - 3)
 #define NEW_LINE (640 * PIXEL_SIZE_GLOBAL)
 #define ALIEN_START_POS 105 + NEW_LINE*105
+#define SPACE_BW_ALIENS 40
 
 static char   white[3]  = {0xFF, 0xFF, 0xFF};
 static char   green[3]  = {0x00, 0xFF, 0x77};
@@ -147,6 +148,44 @@ void draw_sprite(const uint32_t sprite[], uint32_t pos, uint32_t width, uint32_t
     }
 }
 
+void draw_lots_o_aliens(uint32_t pos, uint32_t width, uint32_t height, uint16_t pixel_size, char color[]) {
+    uint32_t currentPoint = pos;
+    uint32_t grid_dimension = pixel_size / PIXEL_SIZE_GLOBAL;
+
+    for (uint16_t alien_y = 0; alien_y < 5; alien_y++) 
+    {
+        char pixels_for_entire_line[(width + SPACE_BW_ALIENS) * grid_dimension * PIXEL_SIZE_GLOBAL * 11];
+        for (uint32_t i = 0; i < height; i++)
+        {
+            for (uint16_t alien_x = 0; alien_x < 11; alien_x++) 
+            {
+                const uint32_t* sprite = alien_army_sprites[alien_y][alien_x];
+                for (uint32_t j = (alien_x*(width+40)); j < (width + SPACE_BW_ALIENS) * 11; j++)
+                {
+                    for (uint32_t x = j * PIXEL_SIZE_GLOBAL * grid_dimension; x < (j + 1) * grid_dimension * PIXEL_SIZE_GLOBAL; x+=PIXEL_SIZE_GLOBAL) 
+                    {
+                        // printf("Setting %d-%d\n\r", x + 0, x + 2);
+                        pixels_for_entire_line[x + 0] = (sprite[i] & (1<<(width-1-j))) && (j < width + (alien_x*(width+40))) ? color[0] : 0x00;
+                        pixels_for_entire_line[x + 1] = (sprite[i] & (1<<(width-1-j))) && (j < width + (alien_x*(width+40))) ? color[1] : 0x00;
+                        pixels_for_entire_line[x + 2] = (sprite[i] & (1<<(width-1-j))) && (j < width + (alien_x*(width+40))) ? color[2] : 0x00;
+                    }
+                    // printf("****INCREMENTING J****\n\n\r");
+                }
+                // printf("****INCREMENTING ALIEN_X****\n\n\r");
+            }
+            for (uint32_t y = 0; y < grid_dimension; y++) 
+            {
+                seek_hdmi(currentPoint+(NEW_LINE)*y);
+                write_hdmi(pixels_for_entire_line, (width + SPACE_BW_ALIENS) * grid_dimension * PIXEL_SIZE_GLOBAL * 11);
+            }
+            currentPoint += NEW_LINE * grid_dimension;
+            seek_hdmi(currentPoint);
+            // printf("****INCREMENTING I****\n\n\r");
+        }
+        currentPoint += NEW_LINE * 15;
+    }
+}
+
 void draw_alien(const uint32_t sprite[], uint32_t pos, uint32_t width, uint32_t height, uint16_t pixel_size, char color[]) {
     uint32_t initPoint = pos; 
     uint32_t currentPoint = initPoint;
@@ -154,20 +193,20 @@ void draw_alien(const uint32_t sprite[], uint32_t pos, uint32_t width, uint32_t 
 
     for (uint32_t i = 0; i < height; i++)
     {
-        char line_of_pixels[width * grid_dimension * PIXEL_SIZE_GLOBAL];
+        char pixels_for_sprite_line[width * grid_dimension * PIXEL_SIZE_GLOBAL];
         for (uint32_t j = 0; j < width; j++)
         {
             for (uint32_t x = j * PIXEL_SIZE_GLOBAL * grid_dimension; x < (j + 1) * grid_dimension * PIXEL_SIZE_GLOBAL; x+=PIXEL_SIZE_GLOBAL) 
             {
-                line_of_pixels[x + 0] = (sprite[i] & (1<<(width-1-j))) ? color[0] : 0x00;
-                line_of_pixels[x + 1] = (sprite[i] & (1<<(width-1-j))) ? color[1] : 0x00;
-                line_of_pixels[x + 2] = (sprite[i] & (1<<(width-1-j))) ? color[2] : 0x00;
+                pixels_for_sprite_line[x + 0] = (sprite[i] & (1<<(width-1-j))) ? color[0] : 0x00;
+                pixels_for_sprite_line[x + 1] = (sprite[i] & (1<<(width-1-j))) ? color[1] : 0x00;
+                pixels_for_sprite_line[x + 2] = (sprite[i] & (1<<(width-1-j))) ? color[2] : 0x00;
             }
         }
         for (uint32_t y = 0; y < grid_dimension; y++) 
         {
             seek_hdmi(currentPoint+(NEW_LINE)*y);
-            write_hdmi(line_of_pixels, width * grid_dimension * PIXEL_SIZE_GLOBAL);
+            write_hdmi(pixels_for_sprite_line, width * grid_dimension * PIXEL_SIZE_GLOBAL);
         }
         currentPoint += NEW_LINE * grid_dimension;
         seek_hdmi(currentPoint);
@@ -637,7 +676,7 @@ void isr_fit()
     }
 
     if ((++alien_move_ctr >= ALIEN_MOVE_MAX_VAL) && !game_over) {
-        move_alien_army_horizontal(moving_right);
+        // move_alien_army_horizontal(moving_right);
         alien_move_ctr = 0;
     }
 }
@@ -681,8 +720,13 @@ int main()
 {
     init_hdmi();
     black_whole_screen();
-    init_alien_army();
+    // init_alien_army();
     // init_alien_army_true();
+    draw_lots_o_aliens(ALIEN_START_POS,
+                        12,
+                        8,
+                        PIXEL_SIZE_GLOBAL * 2,
+                        magenta);
     init_score();
     init_lives();
     init_bunkers();
