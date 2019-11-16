016727b41f1fc3e8b935582eb52a5fc568a899a3
nufsty2 <nufsty2@gmail.com>
2019-11-16 12:06:20 -0700

    Start lab 5

---
+++ vivado/digilent_ip/cache/2017.4/d5203055053e6602/hdmi_out_axis_subset_converter_0_0_stub.v
@@ -0,0 +1,35 @@
+// Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2017.4 (lin64) Build 2086221 Fri Dec 15 20:54:30 MST 2017
+// Date        : Tue Jun 12 12:02:55 2018
+// Host        : goeders-vm running 64-bit Ubuntu 16.04.4 LTS
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ hdmi_out_axis_subset_converter_0_0_stub.v
+// Design      : hdmi_out_axis_subset_converter_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "top_hdmi_out_axis_subset_converter_0_0,Vivado 2017.4" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(aclk, aresetn, s_axis_tvalid, s_axis_tready, 
+  s_axis_tdata, s_axis_tkeep, s_axis_tlast, s_axis_tuser, m_axis_tvalid, m_axis_tready, 
+  m_axis_tdata, m_axis_tkeep, m_axis_tlast, m_axis_tuser)
+/* synthesis syn_black_box black_box_pad_pin="aclk,aresetn,s_axis_tvalid,s_axis_tready,s_axis_tdata[23:0],s_axis_tkeep[2:0],s_axis_tlast,s_axis_tuser[0:0],m_axis_tvalid,m_axis_tready,m_axis_tdata[23:0],m_axis_tkeep[2:0],m_axis_tlast,m_axis_tuser[0:0]" */;
+  input aclk;
+  input aresetn;
+  input s_axis_tvalid;
+  output s_axis_tready;
+  input [23:0]s_axis_tdata;
+  input [2:0]s_axis_tkeep;
+  input s_axis_tlast;
+  input [0:0]s_axis_tuser;
+  output m_axis_tvalid;
+  input m_axis_tready;
+  output [23:0]m_axis_tdata;
+  output [2:0]m_axis_tkeep;
+  output m_axis_tlast;
+  output [0:0]m_axis_tuser;
+endmodule
---
+++ vivado/vivado-boards/utility/Vivado_init.tcl
@@ -0,0 +1,1 @@
+set_param board.repoPaths [list "/home/bjames/pynq/ecen427/vivado-boards/new/board_files"]
